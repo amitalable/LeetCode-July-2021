094c870c47bba005e43acbf0ee1ce3539a2da9ec
Amit Ranjan <amitalable@gmail.com>
2021-07-03 01:47:01 +0530

    Using Stack

--- Tree/Easy/InOrderTraversal.py
+++ Tree/Easy/InOrderTraversal.py
@@ -22,7 +22,32 @@ class Solution:
 
         return result
 
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
+    def inorderTraversalUsingRecursion(self, root: TreeNode) -> List[int]:
         if not root:
             return
         return self.inOrder(root, [])
+
+    def inorderTraversalUsingStack(self, root: TreeNode) -> List[int]:
+        current = root
+        stack, res = [], []  # initialize stack
+        while True:
+            # Reach the left most Node of the current Node
+            if current is not None:
+                # Place pointer to a tree node on the stack
+                # before traversing the node's left subtree
+                stack.append(current)
+                current = current.left
+
+            # BackTrack from the empty subtree and visit the Node
+            # at the top of the stack; however, if the stack is
+            # empty you are done
+            elif(stack):
+                current = stack.pop()
+                res.append(current.val)
+                # We have visited the node and its left
+                # subtree. Now, it's right subtree's turn
+                current = current.right
+
+            else:
+                break
+        return res
