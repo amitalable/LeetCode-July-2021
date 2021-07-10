0c69d8a242952a5db4c2515d0d8b7fdcad0e6ece
Amit Ranjan <amitalable@gmail.com>
2021-07-10 10:25:44 +0530

    BinaryTilt

---
+++ Tree/Easy/BinaryTreeTilt.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/binary-tree-tilt/
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        def helper(root):
+            if not root:
+                return 0
+            lv, rv = helper(root.left), helper(root.right)
+            self.ans += abs(lv - rv)
+            return root.val + lv + rv
+
+        self.ans = 0
+        helper(root)
+        return self.ans
