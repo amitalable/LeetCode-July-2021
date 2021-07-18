bbddcc5cd1381cf23b3ba6136178e744ad50c54a
Amit Ranjan <amitalable@gmail.com>
2021-07-18 21:32:40 +0530

    Day18

---
+++ July-2021/Week-3/ReverseNodesInKGroup.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail, head
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        hair = ListNode(0)
+        hair.next = head
+        pre = hair
+
+        while head:
+            tail = pre
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return hair.next
+            nex = tail.next
+            head, tail = self.reverse(head, tail)
+
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+
+        return hair.next
+
+
+root = ListNode(1)
+root.next = ListNode(2)
+root.next.next = ListNode(3)
+root.next.next.next = ListNode(4)
+root.next.next.next.next = ListNode(5)
+print(Solution().reverseKGroup(root, 2))
