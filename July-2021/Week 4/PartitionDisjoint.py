a8e4e5fcea7624f0c5b770ba02e0bd5ea645d07f
Amit Ranjan <amitalable@gmail.com>
2021-07-22 21:56:14 +0530

    Day 22

---
+++ July-2021/Week 4/PartitionDisjoint.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/partition-array-into-disjoint-intervals
+
+from typing import List
+
+
+class Solution:
+    def partitionDisjoint(self, nums: List[int]) -> int:
+        leftMax = globalMax = nums[0]
+        partition = 0
+        for i in range(1, len(nums)):
+            globalMax = max(globalMax, nums[i])
+            if nums[i] < leftMax:  
+                # If nums[i] < leftMax then nums[i] belong to left subarray, re-partition leftSubArr = nums[0..i]
+                partition = i
+                leftMax = globalMax
+        return partition + 1
