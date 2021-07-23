fb4294b8e62b657fb7a9e5a6a022b27ff1ae92d4
Amit Ranjan <amitalable@gmail.com>
2021-07-23 21:30:26 +0530

    Day23

---
+++ July-2021/Week 4/BinaryTreePruning.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/binary-tree-pruning/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if root.left:
+            root.left = self.pruneTree(root.left)
+        if root.right:
+            root.right = self.pruneTree(root.right)
+        if root.val == 0 and not (root.left or root.right):
+            root = None
+        return root
