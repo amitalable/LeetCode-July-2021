68b524e5d7307278acf0e34d3f157decc5fc3fdd
Amit Ranjan <amitalable@gmail.com>
2021-07-23 07:18:44 +0530

    PartitionDisjoint

--- July-2021/Week 4/PartitionDisjoint.py
+++ July-2021/Week 4/PartitionDisjoint.py
@@ -6,11 +6,13 @@ from typing import List
 class Solution:
     def partitionDisjoint(self, nums: List[int]) -> int:
         leftMax = globalMax = nums[0]
-        partition = 0
+        partition = 1
         for i in range(1, len(nums)):
             globalMax = max(globalMax, nums[i])
-            if nums[i] < leftMax:  
-                # If nums[i] < leftMax then nums[i] belong to left subarray, re-partition leftSubArr = nums[0..i]
+            if nums[i] < leftMax:
+                # If nums[i] < leftMax
+                # then nums[i] belong to left subarray,
+                # re-partition leftSubArr = nums[0..i]
                 partition = i
                 leftMax = globalMax
-        return partition + 1
+        return partition
