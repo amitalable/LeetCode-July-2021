0151e285f6e4d323cfacc731d33caa75486fc919
Amit Ranjan <amitalable@gmail.com>
2021-07-25 23:28:39 +0530

    FlattenBinaryTree

---
+++ LinkedList/Medium/FlattenBinaryTreeToLinkedList.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if root is None or not (root.left or root.right):
+            return root
+
+        left = self.flatten(root.left)
+        right = self.flatten(root.right)
+
+        if left:
+            left.right = root.right
+            root.right = root.left
+            root.left = None
+        return right if right else left
